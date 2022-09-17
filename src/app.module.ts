5583df57d3ccb0fb4e26bf910b0fe7b1917a87b1
Hugo Gallotto <gallottohugo@gmail.com>
2022-09-17 19:08:59 -0300

    Workin on jwt and passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,16 @@ import {
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthSignupDto, AuthLoginDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(dto: AuthSignupDto) {
     try {
       const passwordHash = await argon.hash(dto.password);
@@ -18,8 +24,7 @@ export class AuthService {
       const user = await this.prismaService.user.create({
         data: dto,
       });
-      delete user.password;
-      return user;
+      return await this.jwtSign(user.id, user.username);
     } catch (err) {
       throw new UnprocessableEntityException(err);
     }
@@ -35,7 +40,22 @@ export class AuthService {
     if (!user || !(await argon.verify(user.password, dto.password))) {
       throw new UnauthorizedException('Invalid credentials');
     }
-    delete user.password;
-    return user;
+    return await this.jwtSign(user.id, user.username);
+  }
+
+  private async jwtSign(userId: number, username: string) {
+    const payload = {
+      userId: userId,
+      username: username,
+    };
+
+    const jwtOptions = {
+      secret: this.config.get('JWT_SECRET'),
+      expiresIn: '1h',
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload, jwtOptions),
+    };
   }
 }
