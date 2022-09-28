a04d259474c2b7ab26e6d1f24b4a0c00bf0070c0
Hugo Gallotto <gallottohugo@gmail.com>
2022-09-27 21:00:47 -0300

    Working with users authentication, jwt and guards

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prima: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { userId: number; email: string }) {
+    const user = await this.prima.user.findUnique({
+      where: {
+        id: payload.userId,
+      },
+    });
+    delete user.password;
+    return user;
+  }
+}
