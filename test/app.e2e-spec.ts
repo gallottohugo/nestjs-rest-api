042cd8b382c92742c02ae57eba9492b92b624dfa
Hugo Gallotto <gallottohugo@gmail.com>
2022-10-13 20:38:50 -0300

    Fix e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,12 +3,14 @@ import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
+import { spec } from 'pactum';
 import { AuthLoginDto, AuthSignupDto } from 'src/auth/dto';
 
 describe('Application e2e', () => {
   let app: INestApplication;
   let prisma: PrismaService;
   const serverPort = 5555;
+  const baseUrl = 'http://localhost:5555/api/v1';
 
   beforeAll(async () => {
     const appModule = await Test.createTestingModule({
@@ -16,13 +18,15 @@ describe('Application e2e', () => {
     }).compile();
     app = appModule.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    app.setGlobalPrefix('/api/v1');
     await app.init();
     await app.listen(serverPort);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDB();
 
-    pactum.request.setBaseUrl(`http://localhost:${serverPort}/api/v1`);
+    // setBaseUrl does not work
+    //pactum.request.setBaseUrl(`http://localhost:${serverPort}/api/v1`);
   });
 
   afterAll(() => {
@@ -31,44 +35,51 @@ describe('Application e2e', () => {
 
   describe('Auth', () => {
     describe('Signup', () => {
-      test('Should Signup', () => {
+      it('Should Signup', async () => {
         const reqBody: AuthSignupDto = {
           email: 'gallottohugo@gmail.com',
-          password: '',
+          password: 'test1234',
           username: 'gallottohugo',
           firstName: 'Hugo',
           lastName: 'Gallotto',
         };
-        pactum.spec().post('/auth/signup').withBody(reqBody).expectStatus(201);
+        await spec()
+          .post(`${baseUrl}/auth/signup`)
+          .withBody(reqBody)
+          .expectStatus(201);
       });
 
-      test('Should fail if body is empty', () => {
-        pactum.spec().post('/auth/signup').withBody({}).expectStatus(400);
+      it('Should fail if body is empty', async () => {
+        await spec()
+          .post(`${baseUrl}/auth/signup`)
+          .withBody({})
+          .expectStatus(400);
       });
     });
 
     describe('Login', () => {
       const reqBody: AuthLoginDto = {
-        password: '123456',
         username: 'gallottohugo',
+        password: 'test1234',
       };
 
-      test('Should login', async () => {
-        pactum.spec().post('/auth/login').withBody(reqBody).expectStatus(301);
+      it('Should login', async () => {
+        await spec()
+          .post(`${baseUrl}/auth/login`)
+          .withBody(reqBody)
+          .expectStatus(200);
       });
 
-      test('Should fail if username is empty', () => {
-        pactum
-          .spec()
-          .post('/auth/login')
+      it('Should fail if username is empty', async () => {
+        await spec()
+          .post(`${baseUrl}/auth/login`)
           .withBody({ password: reqBody.password })
           .expectStatus(400);
       });
 
-      test('Should fail if password is empty', () => {
-        pactum
-          .spec()
-          .post('/auth/login')
+      it('Should fail if password is empty', async () => {
+        await spec()
+          .post(`${baseUrl}/auth/login`)
           .withBody({ username: reqBody.username })
           .expectStatus(400);
       });
